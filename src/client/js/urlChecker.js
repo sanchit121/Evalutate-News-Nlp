32e1df9ccd43a28f759f9e19ea8cd424c778b827
Sanchit Agarwal <sanchit121@gmail.com>
2023-06-03 08:08:45 +0530

    Add project

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,12 +1,12 @@
 function checkForUrl(inputText) {
-  var result = inputURL.match(
+  var result = inputText.match(
     /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
   );
 
   if (result == null) {
-    return true;
-  } else {
     return false;
+  } else {
+    return true;
   }
 }
 
