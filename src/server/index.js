045296a4c81a4ad5f5db97e17cfe895b8471db4a
Sanchit Agarwal <sanchit121@gmail.com>
2023-06-10 17:49:44 +0530

    Update comment

--- src/server/index.js
+++ src/server/index.js
@@ -21,7 +21,7 @@ app.get("/", function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-  console.log("Example app listening on port 80801!");
+  console.log("Example app listening on port 8081!");
 });
 
 app.get("/test", function (req, res) {
